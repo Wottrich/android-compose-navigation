225b0629ca2b4183d49025a5e2d8235050d58ce1
wottrich <wottrich78@gmail.com>
2021-05-23 22:16:18 -0300

    feat: RegisterNameActivity.kt changes - add new params to use into screens

--- app/src/main/java/wottrich/github/io/androidnavigationcompose/normalnavigation/RegisterNameFlow.kt
+++ app/src/main/java/wottrich/github/io/androidnavigationcompose/normalnavigation/RegisterNameFlow.kt
@@ -1,18 +1,23 @@
 package wottrich.github.io.androidnavigationcompose.normalnavigation
 
+import androidx.navigation.NavType
+import androidx.navigation.compose.navArgument
 import wottrich.github.io.androidnavigationcompose.navigation.BaseNavigationModelImpl
 
 object RegisterNameFlow {
 
     object RegisterNameProperties : BaseNavigationModelImpl(
-        route = "registerNameFlow/registerName",
-        destinations = mapOf(
-            DetailNameProperties.route to DetailNameProperties
-        )
+        route = "registerNameFlow/registerName"
     )
 
-    object DetailNameProperties: BaseNavigationModelImpl(
-        route = "registerNameFlow/detailName"
-    )
+    object DetailNameProperties : BaseNavigationModelImpl(
+        route = "registerNameFlow/detailName",
+        arguments = listOf(
+            navArgument("value") { type = NavType.StringType }
+        )
+    ) {
+        override val routeWithArgument: String
+            get() = "$route/{value}"
+    }
 
 }
